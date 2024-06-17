0e880639e1d23f2d98d8e82cb80f427eededfa42
Tandy <tandy11993904@gmail.com>
2024-06-17 15:55:58 +0800

    補上記事fragment

---
+++ app/src/main/java/com/example/map_clock_api34/note/Note.java
@@ -0,0 +1,108 @@
+package com.example.map_clock_api34.note;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.map_clock_api34.R;
+
+public class Note extends Fragment {
+
+    private Toolbar toolbar;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_note, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        toolbar = getActivity().findViewById(R.id.toolbar);
+        // 初始化 toolbar
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //建立CardView在toolbar
+        CardView cardViewtitle = new CardView(requireContext());
+        cardViewtitle.setLayoutParams(new CardView.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT,
+                ActionBar.LayoutParams.MATCH_PARENT));
+
+        Drawable drawable = ContextCompat.getDrawable(requireContext(), R.drawable.cardviewtitle_shape);
+        cardViewtitle.setBackground(drawable);
+
+        //建立LinearLayout在CardView等等放圖案和文字
+        LinearLayout linearLayout = new LinearLayout(requireContext());
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        ));
+        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+        //
+        ImageView bookmark = new ImageView(requireContext());
+        bookmark.setImageResource(R.drawable.anya062516);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                100, // 设置宽度为 100 像素
+                100 // 设置高度为 100 像素
+        );
+        params.setMarginStart(10); // 设置左边距
+        bookmark.setLayoutParams(params);
+
+        // 創建TextView
+        TextView bookTitle = new TextView(requireContext());
+        bookTitle.setText("記事");
+        bookTitle.setTextSize(15);
+        bookTitle.setTextColor(getResources().getColor(R.color.green)); // 更改文字颜色
+        bookTitle.setPadding(10, 10, 10, 10); // 设置内边距
+
+        linearLayout.addView(bookmark);
+        linearLayout.addView(bookTitle);
+        cardViewtitle.addView(linearLayout);
+
+        // 將cardview新增到actionBar
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false); // 隐藏原有的標題
+            actionBar.setDisplayShowCustomEnabled(true);
+            actionBar.setCustomView(cardViewtitle, new ActionBar.LayoutParams(
+                    ActionBar.LayoutParams.WRAP_CONTENT,
+                    ActionBar.LayoutParams.WRAP_CONTENT,
+                    Gravity.END));
+            actionBar.show();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        ActionBar actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowCustomEnabled(false);
+            actionBar.setCustomView(null);
+        }
+    }
+}
