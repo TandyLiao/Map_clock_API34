e7bc0afb867ffa7aec3127d8c12dbba33d92d7c2
吳俊廷 <945lack@gmail.com>
2024-06-29 01:18:10 +0800

    CreateLocation 修改了插入的資料庫的錯誤

--- app/src/main/java/com/example/map_clock_api34/home/CreateLocation.java
+++ app/src/main/java/com/example/map_clock_api34/home/CreateLocation.java
@@ -117,7 +117,7 @@ public  class CreateLocation extends Fragment {
                 names = sharedViewModel.getDestinationName(sharedViewModel.getLocationCount());
                 latitudes = sharedViewModel.getLatitude(sharedViewModel.getLocationCount());
                 longitudes = sharedViewModel.getLongitude(sharedViewModel.getLocationCount());
-                SQLiteDatabase db = this.getWritableDatabase();
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
                 for (int i = 0; i < names.length; i++) {
                     if (names!= null && latitudes!= 0 && longitudes!= 0) {
                         ContentValues values = new ContentValues();
