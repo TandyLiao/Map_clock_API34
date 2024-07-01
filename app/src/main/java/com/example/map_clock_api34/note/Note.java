b01d239b183ffed1875bb232cc9b2efc08b7add2
Benson <benson0092@gmail.com>
2024-07-01 23:53:23 +0800

    記事分頁轉換

--- app/src/main/java/com/example/map_clock_api34/note/Note.java
+++ app/src/main/java/com/example/map_clock_api34/note/Note.java
@@ -29,7 +29,7 @@ public class Note extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_note, container, false);
+        View view = inflater.inflate(R.layout.note_fragment_note, container, false);
 
         return view;
     }
