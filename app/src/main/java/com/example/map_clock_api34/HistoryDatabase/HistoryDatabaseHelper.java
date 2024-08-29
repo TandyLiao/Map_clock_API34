76420e758635d91a46a6018efafb7ae4ccac2297
吳俊廷 <945lack@gmail.com>
2024-08-29 12:23:50 +0800

    雙資料庫更新，移除設定表

--- app/src/main/java/com/example/map_clock_api34/HistoryDatabase/HistoryDatabaseHelper.java
+++ app/src/main/java/com/example/map_clock_api34/HistoryDatabase/HistoryDatabaseHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class HistoryDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "map_clock_database";
-    private static final int DATABASE_VERSION = 12;
+    private static final int DATABASE_VERSION = 13;
     public HistoryDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -16,14 +16,14 @@ public class HistoryDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(HistoryTable.CREATE_TABLE);
         db.execSQL(LocationTable.CREATE_TABLE);
-        db.execSQL(SettingTable.CREATE_TABLE);
+        //db.execSQL(SettingTable.CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + HistoryTable.TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS " + LocationTable.TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + SettingTable.TABLE_NAME);
+        //db.execSQL("DROP TABLE IF EXISTS " + SettingTable.TABLE_NAME);
         onCreate(db);
     }
     @Override
@@ -38,7 +38,7 @@ public class HistoryDatabaseHelper extends SQLiteOpenHelper {
         try {
             db.execSQL("DELETE FROM " + HistoryTable.TABLE_NAME);
             db.execSQL("DELETE FROM " + LocationTable.TABLE_NAME);
-            db.execSQL("DELETE FROM " + SettingTable.TABLE_NAME);
+            //db.execSQL("DELETE FROM " + SettingTable.TABLE_NAME);
             db.setTransactionSuccessful();
         } catch (Exception e) {
             e.printStackTrace();
@@ -66,7 +66,6 @@ public class HistoryDatabaseHelper extends SQLiteOpenHelper {
                         + COLUMN_ALARM_NAME + " TEXT,"
                         + COLUMN_ARRANGED_ID + " TEXT,"
                         + COLUMN_SETTING_ID + " INTEGER,"
-                        + "FOREIGN KEY(" + COLUMN_SETTING_ID + ") REFERENCES " + SettingTable.TABLE_NAME + "(" + SettingTable.COLUMN_SETTING_ID + "),"
                         + "FOREIGN KEY(" + COLUMN_LOCATION_ID + ") REFERENCES " + LocationTable.TABLE_NAME + "(" + LocationTable.COLUMN_LOCATION_ID + ")ON DELETE CASCADE"
                         + ")";
     }
@@ -81,6 +80,9 @@ public class HistoryDatabaseHelper extends SQLiteOpenHelper {
         public static final String COLUMN_CITY_NAME = "city_name";
         public static final String COLUMN_AREA_NAME = "area_name";
         public static final String COLUMN_NOTE_INFO = "note_detail";
+        public static final String COLUMN_VIBRATE = "vibrate";
+        public static final String COLUMN_RINGTONE = "ringtone";
+        public static final String COLUMN_notificationTime = "notificationTime";
 
 
         public static final String CREATE_TABLE =
@@ -92,26 +94,10 @@ public class HistoryDatabaseHelper extends SQLiteOpenHelper {
                         + COLUMN_ALARM_NAME + " TEXT,"
                         + COLUMN_CITY_NAME + " TEXT,"
                         + COLUMN_AREA_NAME + " TEXT,"
-                        + COLUMN_NOTE_INFO + " TEXT"
-                        + ")";
-    }
-
-    public static class SettingTable {
-        public static final String TABLE_NAME = "setting";
-        public static final String COLUMN_SETTING_ID = "setting_id";
-        public static final String COLUMN_REMINDER_TIME = "reminder_time";
-        public static final String COLUMN_REMINDER_DISTANCE = "reminder_distance";
-        public static final String COLUMN_VIBRATE = "vibrate";
-        public static final String COLUMN_RINGTONE = "ringtone";
-
-        public static final String CREATE_TABLE =
-                "CREATE TABLE " + TABLE_NAME + "("
-                        + COLUMN_SETTING_ID + " INTEGER PRIMARY KEY,"
-                        + COLUMN_REMINDER_TIME + " TEXT,"
-                        + COLUMN_REMINDER_DISTANCE + " REAL,"
+                        + COLUMN_NOTE_INFO + " TEXT,"
                         + COLUMN_VIBRATE + " INTEGER,"
-                        + COLUMN_RINGTONE + " TEXT"
+                        + COLUMN_RINGTONE + " INTEGER,"
+                        + COLUMN_notificationTime + " INTEGER"
                         + ")";
     }
-
 }
--- app/src/main/java/com/example/map_clock_api34/book/BookDatabaseHelper.java
+++ app/src/main/java/com/example/map_clock_api34/book/BookDatabaseHelper.java
@@ -8,7 +8,7 @@ import android.content.ContentValues;
 public class BookDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "map_clock_book";
-    private static final int DATABASE_VERSION = 7;
+    private static final int DATABASE_VERSION = 8;
 
     public BookDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -17,7 +17,7 @@ public class BookDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(LocationTable2.CREATE_TABLE);
-        db.execSQL(SettingTable.CREATE_TABLE);
+        //db.execSQL(SettingTable.CREATE_TABLE);
         db.execSQL(BookTable.CREATE_TABLE);
     }
     @Override
@@ -30,7 +30,7 @@ public class BookDatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + BookTable.TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS " + LocationTable2.TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + SettingTable.TABLE_NAME);
+        //db.execSQL("DROP TABLE IF EXISTS " + SettingTable.TABLE_NAME);
         onCreate(db);
     }
 
@@ -40,7 +40,7 @@ public class BookDatabaseHelper extends SQLiteOpenHelper {
         try {
             db.execSQL("DELETE FROM " + BookTable.TABLE_NAME);
             db.execSQL("DELETE FROM " + LocationTable2.TABLE_NAME);
-            db.execSQL("DELETE FROM " + SettingTable.TABLE_NAME);
+            //db.execSQL("DELETE FROM " + SettingTable.TABLE_NAME);
             db.setTransactionSuccessful();
         } catch (Exception e) {
             e.printStackTrace();
@@ -76,7 +76,7 @@ public class BookDatabaseHelper extends SQLiteOpenHelper {
                         + COLUMN_ALARM_NAME + " TEXT,"
                         + COLUMN_ARRANGED_ID + " TEXT,"
                         + COLUMN_SETTING_ID + " INTEGER,"
-                        + "FOREIGN KEY(" + COLUMN_SETTING_ID + ") REFERENCES " + SettingTable.TABLE_NAME + "(" + SettingTable.COLUMN_SETTING_ID + "),"
+                        //+ "FOREIGN KEY(" + COLUMN_SETTING_ID + ") REFERENCES " + SettingTable.TABLE_NAME + "(" + SettingTable.COLUMN_SETTING_ID + "),"
                         + "FOREIGN KEY(" + COLUMN_LOCATION_ID + ") REFERENCES " + LocationTable2.TABLE_NAME + "(" + LocationTable2.COLUMN_LOCATION_ID + ")ON DELETE CASCADE"
                         + ")";
     }
@@ -91,6 +91,9 @@ public class BookDatabaseHelper extends SQLiteOpenHelper {
         public static final String COLUMN_CITY_NAME = "city_name";
         public static final String COLUMN_AREA_NAME = "area_name";
         public static final String COLUMN_NOTE_INFO = "note_detail";
+        public static final String COLUMN_VIBRATE = "vibrate";
+        public static final String COLUMN_RINGTONE = "ringtone";
+        public static final String COLUMN_notificationTime = "notificationTime";
 
 
 
@@ -103,27 +106,11 @@ public class BookDatabaseHelper extends SQLiteOpenHelper {
                         + COLUMN_ALARM_NAME + " TEXT,"
                         + COLUMN_CITY_NAME + " TEXT,"
                         + COLUMN_AREA_NAME + " TEXT,"
-                        + COLUMN_NOTE_INFO + " TEXT"
+                        + COLUMN_NOTE_INFO + " TEXT,"
+                        + COLUMN_VIBRATE + " INTEGER,"
+                        + COLUMN_RINGTONE + " INTEGER,"
+                        + COLUMN_notificationTime + " INTEGER"
                         + ")";
     }
-
-
-    public static class SettingTable {
-        public static final String TABLE_NAME = "setting";
-        public static final String COLUMN_SETTING_ID = "setting_id";
-        public static final String COLUMN_REMINDER_TIME = "reminder_time";
-        public static final String COLUMN_REMINDER_DISTANCE = "reminder_distance";
-        public static final String COLUMN_VIBRATE = "vibrate";
-        public static final String COLUMN_RINGTONE = "ringtone";
-
-        public static final String CREATE_TABLE =
-                "CREATE TABLE " + TABLE_NAME + " ("
-                        + COLUMN_SETTING_ID + " INTEGER PRIMARY KEY, "
-                        + COLUMN_REMINDER_TIME + " TEXT, "
-                        + COLUMN_REMINDER_DISTANCE + " REAL, "
-                        + COLUMN_VIBRATE + " INTEGER, "
-                        + COLUMN_RINGTONE + " TEXT)";
-    }
-
 
 }
